e6cb1551cd1d845f78370a6065d78278245b5b03
Ivan <ivan.scarciglia@gmail.com>
2023-08-24 12:04:44 +0200

    new feature test

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
-const DetailsScreen = ({ route, navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text>TODO Details</Text>
-    </View>
-  );
+const DetailsScreen = ({route, navigation}) => {
+    const {item} = route.params;
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>TODO Details</Text>
+            <View style={styles.content}>
+                <View>
+                    <Text style={styles.text}>itemId</Text>
+                    <Text style={styles.text}>Thing todo: </Text>
+                    <Text style={styles.text}>has been done:</Text>
+                </View>
+                <View>
+                    <Text style={styles.text}>{item.id}</Text>
+                    <Text style={styles.text}>{item.todo}</Text>
+                    <Text style={styles.text}>{item.completed ? 'YES' : 'NO'}</Text>
+                </View>
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        paddingTop: 15,
+    },
+    content: {
+        flex: 1,
+        flexDirection: "row",
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginVertical: 10,
+    },
+    text: {
+        marginVertical: 5,
+    },
 })
 
 export default DetailsScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,64 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, FlatList, StatusBar, Pressable} from 'react-native';
 
-const HomeScreen = ({ route, navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text>TODO Home</Text>
-    </View>
-  );
+const HomeScreen = ({route, navigation}) => {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    //console.log(data['todos']);
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/todos?limit=3&skip=10')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+
+    const goToItem = (item) => {
+        navigation.navigate('Details', {
+            item: item
+        })
+    }
+
+    return (
+
+        <View style={styles.container}>
+            <Text style={styles.title}>Todo List</Text>
+            <FlatList
+                contentContainerStyle={styles.listContainer}
+                data={data['todos']}
+                keyExtractor={({id}, index) => id}
+                renderItem={({item}) => (
+                    <Pressable onPress={() => goToItem(item)}>
+                        <Text style={styles.item}>{item.id + '. ' + item.todo}</Text>
+                    </Pressable>
+                )}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 15,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginVertical: 10,
+    },
+    item: {
+        backgroundColor: '#CBE8ED',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 15,
+    }
 })
 
 export default HomeScreen;
