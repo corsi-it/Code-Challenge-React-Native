b17f6ed961eb00c94bd1ed4c1eeb9a80ad4928cb
Christian Serra <castachris@hotmail.it>
2023-08-24 10:57:35 +0200

    Revert "Corsi:"
    This reverts commit bcbf97687435ca4589051987dae7c3103ebdbc29.

--- App.jsx
+++ App.jsx
@@ -1,10 +1,19 @@
-import { NavigationContainer } from "@react-navigation/native";
-import AppNavigator from "./navigation/AppNavigator";
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
-      <AppNavigator />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
